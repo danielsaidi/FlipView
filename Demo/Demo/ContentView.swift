37601bdbfd6124e2585d484f8f54000ba77ce0a0
Daniel Saidi <daniel.saidi@gmail.com>
2025-06-02 10:41:56 +0200

    Add demo

---
+++ Demo/Demo/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  Demo
+//
+//  Created by Daniel Saidi on 2025-06-02.
+//
+
+import FlipView
+import SwiftUI
+
+struct ContentView: View {
+
+    @State private var isFlipped = false
+
+    var body: some View {
+        FlipView(
+            isFlipped: $isFlipped,
+            flipDuration: 100,
+            front: { Card(.green) },
+            back: { Card(.red) }
+        )
+        .padding()
+    }
+}
+
+struct Card: View {
+
+    init(_ color: Color) {
+        self.color = color
+    }
+
+    private let color: Color
+
+    var body: some View {
+        color.cornerRadius(10)
+    }
+}
+
+#Preview {
+    ContentView()
+}
