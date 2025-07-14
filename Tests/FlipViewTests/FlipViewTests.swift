763780403920bcb58f5d460e8a82ee453e4059c9
Daniel Saidi <daniel.saidi@gmail.com>
2025-07-14 10:44:09 +0200

    Fix unit tests

--- Tests/FlipViewTests/FlipViewTests.swift
+++ Tests/FlipViewTests/FlipViewTests.swift
@@ -2,11 +2,16 @@ import FlipKit
 import SwiftUI
 import Testing
 
+@MainActor
 @Test func allPublicTypesCanBeCreated() async throws {
     _ = FlipDirection.down
+    #if os(iOS)
     _ = await FlipGestureViewModifier()
-    _ = await FlipView(
+    #endif
+    _ = FlipViewAnimation.linear
+    _ = FlipView(
         isFlipped: .constant(true),
         content: { _ in Color.green }
     )
+    .flipViewAnimation(.bouncy, duration: 1)
 }
